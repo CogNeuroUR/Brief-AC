6f614048e584135f0d7bc536fff4734608b88493
vrabiutz <syn8yk@gmail.com>
2022-06-28 16:32:48 +0200

    separated dprime function

---
+++ dprime.m
@@ -0,0 +1,25 @@
+function t_stats = dprime(t_stats)
+  % 1) Extract rates
+  % 2) Replace zeros and ones (to prevent infinities)
+  %   Zeros -> 1/(2N); N : max nr. of observation in a group
+  %   Ones  -> 1 - 1/(2N)
+  % 3) Compute d-prime
+  for i=1:height(t_stats)
+    % Convert proportions of 0 and 1 to 1/(2N) and 1-1/(2N)
+    % See Macmillan & Creelman (2005), Page 8.
+    if t_stats.H(i) == 1
+      t_stats.H(i) = 1 - 1/(2*(t_stats.Hits(i) + t_stats.Misses(i)));
+    elseif t_stats.H(i) == 0
+      t_stats.H(i) = 1/((t_stats.Hits(i) + t_stats.Misses(i)));
+    end
+
+    if t_stats.FalseAlarms(i) == 0
+      t_stats.FalseAlarms(i) = 1/((t_stats.FalseAlarms(i) + t_stats.CorrectRejections(i)));
+    elseif t_stats.FalseAlarms(i) == 1
+      t_stats.FalseAlarms(i) = 1 - 1/(2*(t_stats.FalseAlarms(i) + t_stats.CorrectRejections(i)));
+    end
+
+    % Compute d-prime
+    t_stats.dprime(i) = norminv(t_stats.H(i)) - norminv(t_stats.F(i));
+  end
+end
