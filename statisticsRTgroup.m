319b7d3324ce8d2235414e31d8061246feb33753
vrabiutz <syn8yk@gmail.com>
2022-06-30 09:41:41 +0200

    added group RT statistics

--- statisticsRTgroup.m
+++ statisticsRTgroup.m
@@ -1,4 +1,4 @@
-function statisticsRTgroup(ExpInfo, key_yes, key_no, save_plots)
+function groupRT = statisticsRTgroup(save_plots)
 %function [rt_act_con, rt_ctx_con, rt_act_inc, rt_ctx_inc] =...
 %          computeRTstatistics(ExpInfo, key_yes, key_no, make_plots, save_plots)
 % Computes RT statistics (mean & std) per condition for each probe type and
@@ -17,17 +17,18 @@ groupRT = [];
 l_subjects = {};
 
 % iterate over files
+fprintf('Sweeping through files ...\n');
 for i=1:length(l_files)
   path2file = [path_results, l_files(i).name];
   
   % check if of mat-extension
-  [fPath, fName, fExt] = fileparts(l_files(i).name);
+  [~, fName, fExt] = fileparts(l_files(i).name);
   
   switch fExt
     case '.mat'
       % ignore demo-results
       if ~contains(l_files(i).name, 'demo')
-        fprintf('%s : %s\n', l_files(i).name, fExt);
+        fprintf('\tLoading : %s\n', l_files(i).name);
         clear ExpInfo;
         load(path2file, 'ExpInfo');
 
@@ -52,19 +53,10 @@ for i=1:length(l_files)
         statsAI = getRTstats(trialsAI);
         statsCI = getRTstats(trialsCI);
         
-        % TODO
-
-        % 3) Compute d-prime  
-        dprimeAC = dprime(statsAC);
-        dprimeCC = dprime(statsCC);
-        dprimeAI = dprime(statsAI);
-        dprimeCI = dprime(statsCI);
-
-        % 4) Dump into matrix
-        groupDprime = [groupDprime;...
-                       dprimeAC.dprime(:)', dprimeAI.dprime(:)',...
-                       dprimeCC.dprime(:)', dprimeCI.dprime(:)'];
-
+        % 3) Dump RTs ONLY in the matrix as rows (ONE PER SUBJECT)
+        groupRT = [groupRT;...
+                   [statsAC{:,2}], [statsAI{:,2}],...
+                   [statsCC{:,2}], [statsCI{:,2}]];
       end
     otherwise
       continue
@@ -72,136 +64,163 @@ for i=1:length(l_files)
 
 end
 
+%% Define CONDITION NAMES for plotting (+ TABLE (auxiliary!))
+%groupRT = array2table(groupRT); %array2table(zeros(0,24));
+probes = ["AC", "AI", "CC", "CI"];
+times = [2:6 8];
+vars = {};
 
-%% Extract trials for each probe by decoding trials' ASF code
-[t_trialsActionCongruent, t_trialsContextCongruent,...
- t_trialsActionIncongruent, t_trialsContextIncongruent] = getTrialResponses(ExpInfo);
-
-%% Collect RT statistics
-% Extract RT = f(presentation time) by probe type
-rt_act_con = getRTstats(t_trialsActionCongruent);
-rt_ctx_con = getRTstats(t_trialsContextCongruent);
-rt_act_inc = getRTstats(t_trialsActionIncongruent);
-rt_ctx_inc = getRTstats(t_trialsContextIncongruent);
+for iP=1:length(probes)
+  for iT=1:length(times)
+    vars = [vars; sprintf('%s_%d', probes(iP), times(iT))];
+  end
+end
 
 %% ########################################################################
 % Plots [CONGRUENT]
+%% ########################################################################
 if make_plots
   fh = figure;
   
+  % General parameters
+  xfactor = 1000/60;
+  ylimits = [650 1100];
+  xlimits = [1.6 8.4]*xfactor;
+  x = [2:6 8]*xfactor; % in ms
+
+  % PLOT 1 : CONGRUENT (Actions vs Context) ===============================
   subplot(2,2,1);
-  % Plot RT's as a function of presentation time
-  screen_freq = (1/60);
-  factor = screen_freq*1000;
-  ylimits = [500 1500];
-  xlimits = [1.8 8.2]*factor;
-  x = [rt_ctx_con{:, 1}]*factor; % in ms
+  % Define indices for for condition category
+  i1 = [1, 6];         % ACTION Probe & CONGRUENT
+  i2 = [13, 18];       % CONTEXT Probe & CONGRUENT
   
-  % Collect mean RT's for action and context probes
-  yAct = [rt_act_con{:, 2}];
-  yCon = [rt_ctx_con{:, 2}];
-  % Collect RT's standard deviation for action and context probes
-  stdAct = [rt_act_con{:, 3}];
-  stdCon = [rt_ctx_con{:, 3}];
+  data1 = [groupRT(:,i1(1):i1(2))];
+  data2 = [groupRT(:,i2(1):i2(2))];
   
-  e1 = errorbar(x,yAct,stdAct);
+  y1 = mean(data1);
+  y2 = mean(data2);
+  
+  err1 = std(data1) / sqrt(length(data1));
+  err2 = std(data2) / sqrt(length(data2));
+  
+  e1 = errorbar(x, y1, err1);
   hold on
-  e2 = errorbar(x,yCon,stdCon);
+  e2 = errorbar(x, y2, err2);
   
   e1.Marker = "x";
   e2.Marker = "o";
-  
-  title('RT : Action & Context Probes [CONGRUENT]');
-  legend('Actions','Context')
-  xlabel('Presentation time [ms]')
-  ylabel('RT [ms]')
+
+  xticks(x)
+  xticklabels(round(x, 2)) 
   xlim(xlimits)
-  ylim(ylimits);
+  ylim(ylimits)
   
-  % Plot [INCONGRUENT]
+  legend('Actions','Context')
+  stitle = sprintf('RT : CONGRUENT (N=%d)', height(groupRT));
+  title(stitle);
+  xlabel('Conditions')
+  ylabel('RT')
+
+  % PLOT 2 : INCONGRUENT (Actions vs Context) =============================
   subplot(2,2,2);
-  % Plot RT's as a function of presentation time
-  screen_freq = (1/60);
-  factor = screen_freq*1000;
-  x = [rt_act_inc{:, 1}]*factor; % in ms
+  % Define indices for for condition category
+  i1 = [7, 12];         % ACTION Probe & INCONGRUENT
+  i2 = [19, 24];        % CONTEXT Probe & INCONGRUENT
   
-  % Collect mean RT's for action and context probes
-  yAct = [rt_act_inc{:, 2}];
-  yCon = [rt_ctx_inc{:, 2}];
-  % Collect RT's standard deviation for action and context probes
-  stdAct = [rt_act_inc{:, 3}];
-  stdCon = [rt_ctx_inc{:, 3}];
+  data1 = [groupRT(:,i1(1):i1(2))];
+  data2 = [groupRT(:,i2(1):i2(2))];
   
-  e1 = errorbar(x,yAct,stdAct);
+  y1 = mean(data1);
+  y2 = mean(data2);
+  
+  err1 = std(data1) / sqrt(length(data1));
+  err2 = std(data2) / sqrt(length(data2));
+  
+  e1 = errorbar(x, y1, err1);
   hold on
-  e2 = errorbar(x,yCon,stdCon);
+  e2 = errorbar(x, y2, err2);
   
   e1.Marker = "x";
   e2.Marker = "o";
   
-  title('RT : Action & Context Probes [INCONGRUENT]');
+  xticks(x)
+  xticklabels(round(x, 2)) 
+  xlim(xlimits)
+  ylim(ylimits)
+
   legend('Actions','Context')
-  xlabel('Presentation time [ms]')
-  ylabel('RT [ms]')
-  xlim(xlimits)
-  ylim(ylimits);
-  
-  % Plot ACTIONS : congruent vs incongruent
+  stitle = sprintf('RT : INCONGRUENT (N=%d)', height(groupRT));
+  title(stitle);
+  xlabel('Conditions')
+  ylabel('RT')
+
+  % PLOT 3 : ACTIONS (Congruent vs Incongruent) ===========================
   subplot(2,2,3);
-  % Plot RT's as a function of presentation time
-  screen_freq = (1/60);
-  factor = screen_freq*1000;
-  x = [rt_ctx_con{:, 1}]*factor; % in ms
+  % Define indices for for condition category
+  i1 = [1, 6];         % ACTION Probe & Congruent
+  i2 = [7, 12];        % ACTION Probe & Incongruent
   
-  % Collect mean RT's for action and context probes
-  yAct = [rt_act_con{:, 2}];
-  yCon = [rt_act_inc{:, 2}];
-  % Collect RT's standard deviation for action and context probes
-  stdAct = [rt_act_con{:, 3}];
-  stdCon = [rt_act_inc{:, 3}];
+  data1 = [groupRT(:,i1(1):i1(2))];
+  data2 = [groupRT(:,i2(1):i2(2))];
   
-  e1 = errorbar(x,yAct,stdAct);
+  y1 = mean(data1);
+  y2 = mean(data2);
+  
+  err1 = std(data1) / sqrt(length(data1));
+  err2 = std(data2) / sqrt(length(data2));
+  
+  e1 = errorbar(x, y1, err1);
   hold on
-  e2 = errorbar(x,yCon,stdCon);
+  e2 = errorbar(x, y2, err2);
   
   e1.Marker = "x";
   e2.Marker = "o";
   
-  title('RT : Actions');
-  legend('Congruent','Incongruent')
-  xlabel('Presentation time [ms]')
-  ylabel('RT [ms]')
+  xticks(x)
+  xticklabels(round(x, 2)) 
   xlim(xlimits)
-  ylim(ylimits);
+  ylim(ylimits)
 
-  % Plot CONTEXT : congruent vs incongruent
+  legend('Congruent','Incongruent')
+  stitle = sprintf('RT : ACTIONS (N=%d)', height(groupRT));
+  title(stitle);
+  xlabel('Conditions')
+  ylabel('RT')
+
+  % PLOT 4 : CONTEXT (Congruent vs Incongruent) ===========================
   subplot(2,2,4);
-  % Plot RT's as a function of presentation time
-  screen_freq = (1/60);
-  factor = screen_freq*1000;
-  x = [rt_ctx_con{:, 1}]*factor; % in ms
+  % Define indices for for condition category
+  i1 = [13, 18];         % CONTEXT Probe & Congruent
+  i2 = [19, 24];        % CONTEXT Probe & Incongruent
   
-  % Collect mean RT's for action and context probes
-  yAct = [rt_ctx_con{:, 2}];
-  yCon = [rt_ctx_inc{:, 2}];
-  % Collect RT's standard deviation for action and context probes
-  stdAct = [rt_act_con{:, 3}];
-  stdCon = [rt_ctx_inc{:, 3}];
+  data1 = [groupRT(:,i1(1):i1(2))];
+  data2 = [groupRT(:,i2(1):i2(2))];
   
-  e1 = errorbar(x,yAct,stdAct);
+  y1 = mean(data1);
+  y2 = mean(data2);
+  
+  err1 = std(data1) / sqrt(length(data1));
+  err2 = std(data2) / sqrt(length(data2));
+  
+  e1 = errorbar(x, y1, err1);
   hold on
-  e2 = errorbar(x,yCon,stdCon);
+  e2 = errorbar(x, y2, err2);
   
   e1.Marker = "x";
   e2.Marker = "o";
   
-  title('RTs : Contexts');
-  legend('Congruent','Incongruent')
-  xlabel('Presentation time [ms]')
-  ylabel('RT [ms]')
+  xticks(x)
+  xticklabels(round(x, 2)) 
   xlim(xlimits)
-  ylim(ylimits);
+  ylim(ylimits)
 
+  legend('Congruent','Incongruent')
+  stitle = sprintf('RT : CONTEXT (N=%d)', height(groupRT));
+  title(stitle);
+  xlabel('Conditions')
+  ylabel('RT')
+
+  % SAVE PLOTS ============================================================
   if save_plots
      % define resolution figure to be saved in dpi
    res = 420;
@@ -209,32 +228,31 @@ if make_plots
    set(fh,'PaperPositionMode','manual')
    fh.PaperUnits = 'inches';
    fh.PaperPosition = [0 0 5000 2500]/res;
-   print('-dpng','-r300',['plots/' ExpInfo.Cfg.name '_RT_statistics'])
+   print('-dpng','-r300',['plots/group_RT_statistics'])
   end
+end % if make_plots
 end
-
-end
-
-
 
 %% ------------------------------------------------------------------------
 function stats = getRTstats(t_trials)
   % t_trials : table
   % iterate over unique values in PresTime and compute mean & std for each
-  fprintf('Collecting RTs...\n')
+  %fprintf('Collecting RTs...\n')
 
   stats = {};
   uniqTimes = unique(t_trials.PresTime);
   
-  fprintf('\nTarget duration: mean & std RT\n')
+  %fprintf('\nTarget duration: mean & std RT\n')
   for i=1:length(uniqTimes)
     values = t_trials.RT(t_trials.PresTime==uniqTimes(i));
     if isequal(class(values), 'cell'); values = cell2mat(values); end
     avg = nanmean(values);
     %stdev = nanstd(values); % SD
     stderr = nanstd(values) / sqrt(length(values)); % SE : standard error
-    fprintf('PresTime: %d; Mean RT: %.2fms; SE RT: %.2fms\n',...
-            uniqTimes(i), avg, stderr);
+    % Verbose
+    %fprintf('PresTime: %d; Mean RT: %.2fms; SE RT: %.2fms\n',...
+    %        uniqTimes(i), avg, stderr);
+    
     stats(end+1, :) = {uniqTimes(i), avg, stderr};
   end
 end
