a8925816435170dc06735f3003270d7d113c23de
vrabiutz <syn8yk@gmail.com>
2022-09-05 15:15:35 +0200

    added some dignostic measures

--- getResponseStats.m
+++ getResponseStats.m
@@ -53,16 +53,35 @@ function t_stats = getResponseStats(tTrials, key_yes, key_no)
     n_empty = 0;
     corr_rejections = 0;
     f_alarms = 0;
+
+    % temp
+    n_correct = 0;
+
+    % temp 2
+    count_trueYes = 0;
+    count_trueNo = 0;
     
     for j=1:length(ResKeys)
       n_samples = n_samples + 1;
+
+      % temp
+      if ResKeys(j) == TrueKeys(j)
+        n_correct = n_correct + 1;
+      end
+
+      % temp 2
+      if key_yes == TrueKeys(j)
+        count_trueYes = count_trueYes + 1;
+      elseif key_no == TrueKeys(j)
+        count_trueNo = count_trueNo + 1;
+      end
+
       
       switch ResKeys(j)
         case key_yes % Targets ("yes" : correct response)
           if key_yes == TrueKeys(j) % Hit
             hits = hits + 1;
           else % False alarm : "yes" when NO
-            %misses = misses + 1;
             f_alarms = f_alarms + 1;
           end
         
@@ -70,7 +89,6 @@ function t_stats = getResponseStats(tTrials, key_yes, key_no)
           if key_no == TrueKeys(j)  % correct rejection
             corr_rejections = corr_rejections + 1;
           else % Miss : "no" when YES
-            %f_alarms = f_alarms + 1;
             misses = misses + 1;
           end
           
@@ -79,12 +97,19 @@ function t_stats = getResponseStats(tTrials, key_yes, key_no)
       end % switch
 
     end
+
+    % temp
+    rate_correct = n_correct / length(ResKeys);
+
+    % temp 2
+    rate_yesVno = count_trueYes / count_trueNo;
+    %fprintf('\t%d / %d (yes/no)\n', count_trueYes, count_trueNo)
     
     % Subtract empty trials
     n_samples = n_samples - n_empty;
-    if n_empty ~= 0
-      fprintf('\tCounted missing responses: %d.\n', n_empty)
-    end
+%     if n_empty ~= 0
+%       fprintf('\tCounted missing responses: %d.\n', n_empty)
+%     end
 
     % compute hit- and false alarm rates
     if hits ~= 0
@@ -101,10 +126,12 @@ function t_stats = getResponseStats(tTrials, key_yes, key_no)
     % concatenate
     stats(end+1, :) = {uniqTimes(i), n_samples,...
                        hits, hit_rate, misses,...
-                       f_alarms, f_alarm_rate, corr_rejections};
+                       f_alarms, f_alarm_rate, corr_rejections,...
+                       rate_correct, rate_yesVno};
 
     % Convert to table
-    varnames = {'PresTime' 'N_samples' 'Hits' 'H' 'Misses' 'FalseAlarms' 'F' 'CorrectRejections'};
+    varnames = {'PresTime' 'N_samples' 'Hits' 'H' 'Misses' 'FalseAlarms'...
+                'F' 'CorrectRejections' 'PercentCorrect' 'RateYesNo'};
     t_stats = cell2table(stats, 'VariableNames', varnames);
   end
 end
